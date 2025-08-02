a230142f13edcfb15153d7e4be91b60842d0ef37
Sia <siavash_p@hotmail.com>
2025-08-02 14:02:48 -0700

    Adding changes to service

--- src/app/services/svg.service.ts
+++ src/app/services/svg.service.ts
@@ -1,38 +1,29 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { timer, Subject, BehaviorSubject, Observable } from 'rxjs';
+import { takeUntil, tap, finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SvgService {
 
-  private svgSubject = new Subject<string>();
+  private svgSubject = new BehaviorSubject<string | null>(null);
   svg$ = this.svgSubject.asObservable();
-  
 
   constructor(private httpClient: HttpClient) {}
 
-  getInitalsInSVG(name:string){
+  getInitalsInSVG(name: string): Observable<string> {
+
     const url = "https://tagdiscovery.com/api/get-initials";
     let params = new HttpParams()
-    .set('name', name)
-    .set('delay', 10);
-
-
-    this.httpClient.get(url, { params, responseType: 'text' })
-    .subscribe(response => {
-      this.svgSubject.next(response)
-      console.log(response);
-    });
-
+      .set('name', name)
+      .set('delay', 10);
+      
+      return this.httpClient.get(url, { params,responseType: 'text' }).pipe(
+        tap(svg => this.svgSubject.next(svg))
+      );
   }
-
-
-
-  
-
-
 
 
 }
